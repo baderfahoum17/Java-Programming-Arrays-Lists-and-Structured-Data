f9cbbd75f0bea4ebe429a3cc325f803ce067ea7c
bader fahoum <fahoumbader@gmail.com>
2020-05-06 18:48:15 +0300

    Create Tester.java

---
+++ Week3/1) Reading-Log-Files/Tester.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+/**
+ * Write a description of class Tester here.
+ *
+ * @author (Bader Fahoum)
+ * @version (2020)
+ */
+public class Tester
+{
+    public void testLogEntry() {
+        LogEntry le = new LogEntry("1.2.3.4", new Date(), "example request", 200, 500);
+        System.out.println(le);
+        LogEntry le2 = new LogEntry("1.2.100.4", new Date(), "example request 2", 300, 400);
+        System.out.println(le2);
+    }
+
+    public void testLogAnalyzer() {
+        LogAnalyzer la = new LogAnalyzer();
+        la.readFile("short-test_log");
+        la.printAll();
+    }
+}
