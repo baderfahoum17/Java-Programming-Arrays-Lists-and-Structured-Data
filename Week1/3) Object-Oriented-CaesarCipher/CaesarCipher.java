ffd40d6cb18afbbb77e3bbb80b7edd8ea965e29d
bader fahoum <fahoumbader@gmail.com>
2020-05-06 18:36:40 +0300

    Create CaesarCipher.java

---
+++ Week1/3) Object-Oriented-CaesarCipher/CaesarCipher.java
@@ -0,0 +1,37 @@
+public class CaesarCipher {
+/**
+ * This class belongs to the Assignment 1 exercise. For mor info, 
+ * 
+ * 
+ * @author (Bader Fahoum) 
+ * @version (2020)
+ */
+    private String alphabet;
+    private String shiftedAlphabet;
+    private int mainKey;
+
+
+    public CaesarCipher(int key) {
+        this.alphabet = "abcdefghijklmnopqrstuvwxyz";
+        this.shiftedAlphabet = this.alphabet.substring(key)+
+                this.alphabet.substring(0,key);
+        this.mainKey = key;
+    }
+    public String encrypt(String input) {
+        StringBuilder encrypted = new StringBuilder(input);
+        for(int i = 0; i < encrypted.length(); i++) {
+            char currChar = encrypted.charAt(i);
+            if(this.alphabet.indexOf(currChar) != -1){
+                int idx = this.alphabet.indexOf(currChar);
+                char newChar = this.shiftedAlphabet.charAt(idx);
+                encrypted.setCharAt(i, newChar);
+            }
+        }
+        return encrypted.toString();
+    }
+
+    public String decrypt(String input) {
+        CaesarCipher cc = new CaesarCipher(26 - mainKey);
+        return cc.encrypt(input);
+    }
+}
